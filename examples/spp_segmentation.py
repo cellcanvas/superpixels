025e481310d3e14ac13d080a3f2f7157c3ee0090
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2024-09-17 14:37:50 -0700

    adding interactive mode

--- examples/spp_segmentation.py
+++ examples/spp_segmentation.py
@@ -3,7 +3,10 @@ import zarr
 import napari
 import numpy as np
 import copick
+import click
 import copick_utils
+from magicgui import magicgui
+from napari.types import ImageData, LabelsData
 from tifffile import imwrite
 from pathlib import Path
 
@@ -19,7 +22,22 @@ except KeyError:
     )
 
 
-def main() -> None:
+@magicgui(auto_call=True, sigma={"widget_type": "FloatSlider", "min": 0, "max": 10}, h_minima={"widget_type": "FloatSlider", "min": 0, "max": 50})
+def _spp_widget(image: ImageData, sigma: float = 4, h_minima: float = 0.001) -> LabelsData:
+    return superpixels(image, sigma=sigma, h_minima=h_minima)
+
+# REF: https://github.com/pyapp-kit/magicgui/issues/226
+_spp_widget.h_minima._widget._readout_widget.setDecimals(4)
+
+
+@click.command()
+@click.option("--interactive", type=bool, is_flag=True, default=False, help="Run in interactive mode")
+def main(interactive: bool) -> None:
+    """
+    Run the superpixel segmentation on a tomogram from the copick dataset.
+    Usage:
+        python spp_segmentation.py <OPTIONAL --interactive>
+    """
 
     # Use copick
     # config_file = "./copick_10301/synthetic_data_10301_dataportal.json"
@@ -38,6 +56,10 @@ def main() -> None:
     z = zarr.open(tomogram.zarr())
     img = z["0"] # Get our highest resolution scale
 
+    if interactive:
+        # cropping when interactive, because segmentation is slow
+        img = img[50:100, 180:360, 210:430]
+
     print("Loading image into memory ...")
     img = np.asarray(img) # Loading into memory
 
@@ -45,16 +67,18 @@ def main() -> None:
     segm = superpixels(img, sigma=4, h_minima=0.0025)
     print("Done ...")
 
-    # uncomment to save the segmentation
-    # imwrite("segm.tif", segm)
-
     # Open viewer and add image
     viewer = napari.Viewer()
     viewer.add_image(img)
     viewer.add_labels(segm)
 
-    napari.run()
+    if interactive:
+        viewer.window.add_dock_widget(_spp_widget, area="right")
 
+    # uncomment to save the segmentation
+    # imwrite("segm.tif", segm)
+
+    napari.run()
 
 
 if __name__ == "__main__":
