d6b85afcc8cee9efdb38bc65ee0b7f43dda086cb
zhuowenzhao <zwenzhao11@gmail.com>
2024-09-18 11:25:59 -0700

    added feature funcs

--- src/cellcanvas_spp/features.py
+++ src/cellcanvas_spp/features.py
@@ -1,9 +1,54 @@
 import pandas as pd
 from numpy.typing import ArrayLike
+from skimage.measure import regionprops_table
+import numpy as np
 
 
 def superpixel_regionprops_features(
     image: ArrayLike,
     superpixels: ArrayLike,
 ) -> pd.DataFrame:
-    pass
+    properties = [
+        'label',
+        'area',
+        'bbox',
+        'bbox_area',
+        'centroid',
+        'equivalent_diameter',
+        'euler_number',
+        'extent',
+        'filled_area',
+        'major_axis_length',
+        'max_intensity',
+        'mean_intensity',
+        'min_intensity',
+        'std_intensity',
+    ]
+    df = pd.DataFrame(
+        regionprops_table(
+            label_image=superpixels,
+            intensity_image=image,
+            properties=properties,
+        )
+    )
+    return  df
+
+
+def superpixel_cellcanvas_features(
+    embeddings: ArrayLike,
+    superpixels: ArrayLike,
+    embedding_axis: int = 0,
+) -> pd.DataFrame:
+    """
+    Retun the median of the embeddings for each superpixel.
+    """
+    if embedding_axis != len(embeddings.shape) - 1:
+        embeddings = np.moveaxis(embeddings, embedding_axis, -1)
+    
+    dt = dict()
+    labels = np.unique(superpixels)
+    for label in labels:
+        ssp= np.median(embeddings[superpixels==label], axis=0)
+        dt[label] = ssp
+
+    return  pd.DataFrame(dt)
